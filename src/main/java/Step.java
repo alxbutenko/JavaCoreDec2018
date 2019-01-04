baf105b9cd8f261d4cf0ec9d0626d70b43323869
Alexander Butenko <boooff@gmail.com>
2019-01-05 02:43:42 +0300

    add "step" input checking

--- src/main/java/Step.java
+++ src/main/java/Step.java
@@ -7,7 +7,17 @@ public class Step {
             try {
                 BufferedReader reader = new BufferedReader (new InputStreamReader (System.in));
                 System.out.print ("Please enter step: ");
-                return Double.parseDouble (reader.readLine ());
+                double step = Double.parseDouble (reader.readLine ());
+
+                if (step <= 0) {
+                    throw new IllegalStateException ();
+                }
+                else {
+                    return step;
+                }
+
+            } catch (IllegalStateException ex) {
+                System.out.println ("Step must be > 0!");
             } catch (Exception ex) {
                 System.out.println ("Please enter number in a valid format (int or double)!");
             }
